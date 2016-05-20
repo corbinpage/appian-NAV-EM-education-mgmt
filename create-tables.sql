5f81e968cf582304115b27ac3466d750517e29b1
Corbin Page <corbin.page@gmail.com>
2016-05-19 22:14:49 -0400

    Working on courses

--- create-tables.sql
+++ create-tables.sql
@@ -6,11 +6,32 @@ DROP TABLE IF EXISTS `nav_em_student`;
 
 CREATE TABLE `nav_em_student` (
   `id` int(11) NOT NULL,
-  `student_id` int(11) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `address_1` varchar(255) DEFAULT NULL,
-  `city` varchar(255) DEFAULT NULL
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `postal` int(11) NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `dob` DATE DEFAULT NULL,
+  `home_phone` varchar(255) DEFAULT NULL,
+  `cell` varchar(255) DEFAULT NULL,
+  `ssn` varchar(255) DEFAULT NULL,
+  `pic_large` varchar(255) DEFAULT NULL,
+  `pic_medium` varchar(255) DEFAULT NULL,
+  `pic_small` varchar(255) DEFAULT NULL,
+  `nat` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `customText1` varchar(255) DEFAULT NULL,
+  `customText2` varchar(255) DEFAULT NULL,
+  `customText3` TEXT DEFAULT NULL,
+  `customText4` TEXT DEFAULT NULL,
+  `customText5` TEXT DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `nav_em_student`
@@ -18,6 +39,31 @@ ALTER TABLE `nav_em_student`
 
 ALTER TABLE `nav_em_student`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+
+DROP TABLE IF EXISTS `nav_em_course`;
+
+CREATE TABLE `nav_em_course` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `desciption` TEXT DEFAULT NULL,
+  `objective` TEXT DEFAULT NULL,
+  `school` varchar(255) DEFAULT NULL,
+  `year` varchar(255) DEFAULT NULL,
+  `level` varchar(255) DEFAULT NULL,
+  `start_date` DATE DEFAULT NULL,
+  `end_date` DATE DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `nav_em_course`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `nav_em_course`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
 
 
 DROP TABLE IF EXISTS `nav_em_session`;
@@ -30,7 +76,7 @@ CREATE TABLE `nav_em_session` (
   `level` int(11) DEFAULT NULL,
   `start_date` DATE DEFAULT NULL,
   `end_date` DATE DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `nav_em_session`
